a4bfd0780088aa5e77b1cdc4c0500a190e531305
A <krishpatel6865@gmail.com>
2026-06-25 14:01:02 +0530

    initial

---
+++ src/p11.kt
@@ -0,0 +1,24 @@
+fun main (){
+    var i = 200
+    println("i =" + i)
+    var j : Int = 100
+    println("Integer value =" + j)
+    var k : Float = 1.5f
+    println("Float value =" + k)
+    var l : Char = 'k'
+    println("Character value =" + l)
+    var m : String = "JAY MADI"
+    println("String value =" + m)
+    var n : Boolean = true
+    println("Boolean value =" + n)
+    var o : Byte = 123
+    println("Byte value =" + o)
+    var p : Long = 12333L
+    println("Long value =" + p)
+    var q : Double = 12.34
+    println("Double value =" + q)
+    var r : Short = -2430
+    val z = 20000
+    println("Short value =" + z)
+    println("Short value =" + r)
+}
